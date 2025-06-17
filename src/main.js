993c64158433926824f1d2e729a66a24ed30ab55
CSXV <maliksharif@hotmail.com>
2025-06-17 18:14:33 +0300

    imporve the code

--- src/main.js
+++ src/main.js
@@ -95,45 +95,16 @@ class clsWeather {
 let _GridYInput = document.getElementById("GridY-input");
 let _GridXInput = document.getElementById("GridX-input");
 let _OfficeIdInput = document.getElementById("OfficeId-input");
+
 let _xGrid = _GridXInput.value;
 let _yGrid = _GridYInput.value;
 let _officeID = _OfficeIdInput.value;
 
-// Temps ----------------------------
-let _todayTemps = document.getElementById("today-temps");
-let _tomorrowTemps = document.getElementById("tomorrow-temps");
-let _nextTomorrowTemps = document.getElementById("next-tomorrow-temps");
-let _fourthTemps = document.getElementById("fourth-temps");
-let _fifthTemps = document.getElementById("fifth-temps");
-let _sixthTemps = document.getElementById("sixth-temps");
-let _seventhTemps = document.getElementById("seventh-temps");
-
-// Forecast ----------------------------
-let _todayForecast = document.getElementById("today-forecast");
-let _tomorrowForecast = document.getElementById("tomorrow-forecast");
-let _nextTomorrowForecast = document.getElementById("next-tomorrow-forecast");
-let _fourthForecast = document.getElementById("fourth-forecast");
-let _fifthForecast = document.getElementById("fifth-forecast");
-let _sixthForecast = document.getElementById("sixth-forecast");
-let _seventhForecast = document.getElementById("seventh-forecast");
-
-// Header ----------------------------
-let _todayHeader = document.getElementById("today-header");
-let _tomorrowHeader = document.getElementById("tomorrow-header");
-let _nextTomorrowHeader = document.getElementById("next-tomorrow-header");
-let _fourthHeader = document.getElementById("fourth-header");
-let _fifthHeader = document.getElementById("fifth-header");
-let _sixthHeader = document.getElementById("sixth-header");
-let _seventhHeader = document.getElementById("seventh-header");
-
-// icons ----------------------------
-let _todayIcon = document.getElementById("today-icon");
-let _tomorrowIcon = document.getElementById("tomorrow-icon");
-let _nextTomorrowIcon = document.getElementById("next-tomorrow-icon");
-let _fourthIcon = document.getElementById("fourth-icon");
-let _fifthIcon = document.getElementById("fifth-icon");
-let _sixthIcon = document.getElementById("sixth-icon");
-let _seventhIcon = document.getElementById("seventh-icon");
+// days ----------------------------
+let DaysTemps = document.querySelectorAll(".DaysTemps");
+let DaysForecast = document.querySelectorAll(".weather-info-forecast");
+let DaysText = document.querySelectorAll(".DaysText");
+let DaysIcons = document.querySelectorAll(".DaysIcons");
 
 // precipitation ----------------------------
 let _todayPrecipitation = document.getElementById("today-precipitation");
@@ -141,65 +112,70 @@ let _todayDewPoint = document.getElementById("today-DewPoint");
 let _todayHumidity = document.getElementById("today-humidity");
 
 // Wind ----------------------------
-let _todayWind = document.getElementById("today-wind");
 let _todayWindSpeed = document.getElementById("today-wind-Speed");
 let _todayWindDirection = document.getElementById("today-wind-Direction");
 let _todayWindIcon = document.getElementById("wind-icon");
 
 // today's forecast ----------------------------
-// today's header ----------------------------
-let _todaysHeader1 = document.getElementById("todays-header-1");
-let _todaysHeader2 = document.getElementById("todays-header-2");
-let _todaysHeader3 = document.getElementById("todays-header-3");
-let _todaysHeader4 = document.getElementById("todays-header-4");
-let _todaysHeader5 = document.getElementById("todays-header-5");
-let _todaysHeader6 = document.getElementById("todays-header-6");
+let HourlyText = document.querySelectorAll(".HourlyText");
+let HourlyTemps = document.querySelectorAll(".HourlyTemps");
+let HourlyIcon = document.querySelectorAll(".HourlyIcon");
 
-// today's temps ----------------------------
-let _todaysTemp1 = document.getElementById("todays-temp-1");
-let _todaysTemp2 = document.getElementById("todays-temp-2");
-let _todaysTemp3 = document.getElementById("todays-temp-3");
-let _todaysTemp4 = document.getElementById("todays-temp-4");
-let _todaysTemp5 = document.getElementById("todays-temp-5");
-let _todaysTemp6 = document.getElementById("todays-temp-6");
-
-// today's temps ----------------------------
-let _todaysIcon1 = document.getElementById("todays-icon-1");
-let _todaysIcon2 = document.getElementById("todays-icon-2");
-let _todaysIcon3 = document.getElementById("todays-icon-3");
-let _todaysIcon4 = document.getElementById("todays-icon-4");
-let _todaysIcon5 = document.getElementById("todays-icon-5");
-let _todaysIcon6 = document.getElementById("todays-icon-6");
-
-// v ----------------------------
+// APIs ----------------------------
 let APIstring =
   "https://corsproxy.io/?" +
   // `https://api.weather.gov/gridpoints/OKX/35,35/forecast?units=si`;
   `https://api.weather.gov/gridpoints/${_officeID}/${_xGrid},${_yGrid}/forecast?units=si`;
 
-let TodayAPIstring =
+let HourlyAPIstring =
   "https://corsproxy.io/?" +
   // `https://api.weather.gov/gridpoints/OKX/35,35/forecast?units=si`;
   `https://api.weather.gov/gridpoints/${_officeID}/${_xGrid},${_yGrid}/forecast/hourly?units=si`;
 
 // ----------------------------------------------------------------------------------------------
-// functions
+// events
 // ----------------------------------------------------------------------------------------------
 _GridXInput.addEventListener("change", (event) => {
-  _xGrid = event.target.value;
+  const xGrid = event.target.value;
 
-  console.log(_xGrid);
+  if (!checkForNumbersOnly(xGrid)) {
+    event.target.style.backgroundColor = "lightcoral";
+    return;
+  } else {
+    event.target.style.backgroundColor = "";
+  }
+
+  _xGrid = xGrid;
+
   changeAPI();
 });
 
 _GridYInput.addEventListener("change", (event) => {
-  _yGrid = event.target.value;
+  const yGrid = event.target.value;
+
+  if (!checkForNumbersOnly(yGrid)) {
+    event.target.style.backgroundColor = "lightcoral";
+    return;
+  } else {
+    event.target.style.backgroundColor = "";
+  }
+
+  _yGrid = yGrid;
 
   changeAPI();
 });
 
 _OfficeIdInput.addEventListener("change", (event) => {
-  _officeID = event.target.value;
+  const ID = event.target.value;
+
+  if (!checkForLettersOnly(ID)) {
+    event.target.style.backgroundColor = "lightcoral";
+    return;
+  } else {
+    event.target.style.backgroundColor = "";
+  }
+
+  _officeID = ID;
 
   changeAPI();
 });
@@ -207,35 +183,50 @@ _OfficeIdInput.addEventListener("change", (event) => {
 // ----------------------------------------------------------------------------------------------
 // functions
 // ----------------------------------------------------------------------------------------------
+function checkForLettersOnly(string) {
+  return /^[a-zA-Z]+$/.test(string);
+}
+
+function checkForNumbersOnly(numbers) {
+  return /^[0-9]+$/.test(numbers);
+  // return typeof numbers === "number";
+}
+
 function changeAPI() {
+  if (_xGrid == "" || _yGrid == "" || _officeID == "") return;
+
   APIstring =
     "https://corsproxy.io/?" +
     `https://api.weather.gov/gridpoints/${_officeID}/${_xGrid},${_yGrid}/forecast?units=si`;
 
-  TodayAPIstring =
+  HourlyAPIstring =
     "https://corsproxy.io/?" +
     `https://api.weather.gov/gridpoints/${_officeID}/${_xGrid},${_yGrid}/forecast/hourly?units=si`;
 
   init();
 }
 
-function fetchWeatherDataFromAPI(src) {
-  return new Promise(function(resolve, reject) {
-    // fetch("https://www.api.weather.gov/gridpoints/OKX/35,35/forecast")
-    fetch(src)
+function fetchWeatherDataFromAPI(src, timeout = "5000") {
+  const controller = new AbortController();
+  const timer = setTimeout(() => controller.abort(), timeout);
+
+  return new Promise(function(resolve) {
+    fetch(src, { signal: controller.signal })
       .then((response) => response.json())
-      // .then((data) => resolve(data.properties.periods[1].shortForecast));
-      .then((data) => resolve(praiseData(data)));
+      .then((data) => resolve(praiseData(data)))
+      .finally(() => clearTimeout(timer));
   });
 }
 
-function fetchHourlyWeatherDataFromAPI(src) {
-  return new Promise(function(resolve, reject) {
-    // fetch("https://www.api.weather.gov/gridpoints/OKX/35,35/forecast")
-    fetch(src)
+function fetchHourlyWeatherDataFromAPI(src, timeout = "5000") {
+  const controller = new AbortController();
+  const timer = setTimeout(() => controller.abort(), timeout);
+
+  return new Promise(function(resolve) {
+    fetch(src, { signal: controller.signal })
       .then((response) => response.json())
-      // .then((data) => resolve(data.properties.periods[1].shortForecast));
-      .then((data) => resolve(praiseHourlyData(data)));
+      .then((data) => resolve(praiseHourlyData(data)))
+      .finally(() => clearTimeout(timer));
   });
 }
 
@@ -244,21 +235,22 @@ function praiseHourlyData(data) {
 
   for (let i = 0; i < 7; i++) {
     let hourlyData = new clsHourlyWeather(
-      data.properties.periods[i].startTime,
-      data.properties.periods[i].temperature,
-      data.properties.periods[i].temperatureUnit,
-      data.properties.periods[i].probabilityOfPrecipitation.value,
-      data.properties.periods[i].windSpeed,
-      data.properties.periods[i].windDirection,
-      data.properties.periods[i].shortForecast,
-      data.properties.periods[i].dewpoint.value,
-      data.properties.periods[i].relativeHumidity.value,
+      data?.properties?.periods[i]?.startTime || "unknown",
+      data?.properties?.periods[i]?.temperature || "unknown",
+      data?.properties?.periods[i]?.temperatureUnit || "unknown",
+      data?.properties?.periods[i]?.probabilityOfPrecipitation?.value ||
+      "unknown",
+      data?.properties?.periods[i]?.windSpeed || "unknown",
+      data?.properties?.periods[i]?.windDirection || "unknown",
+      data?.properties?.periods[i]?.shortForecast || "unknown",
+      data?.properties?.periods[i]?.dewpoint.value || "unknown",
+      data?.properties?.periods[i]?.relativeHumidity?.value || "unknown",
     );
 
     hourlyDataArray.push(hourlyData);
   }
 
-  console.log(hourlyDataArray);
+  // console.log(hourlyDataArray);
   return hourlyDataArray;
 }
 
@@ -267,15 +259,16 @@ function praiseData(data) {
   let Nights = [];
 
   for (let i = 0; i <= 13; i++) {
-    if (data.properties.periods[i].isDaytime == true) {
+    if (data?.properties?.periods[i]?.isDaytime == true) {
       let day = new clsweatherDays(
-        data.properties.periods[i].temperature,
-        data.properties.periods[i].temperatureUnit,
-        data.properties.periods[i].name,
-        data.properties.periods[i].shortForecast,
-        data.properties.periods[i].probabilityOfPrecipitation.value,
-        data.properties.periods[i].windSpeed,
-        data.properties.periods[i].windDirection,
+        data?.properties?.periods[i]?.temperature || "unknown",
+        data?.properties?.periods[i]?.temperatureUnit || "unknown",
+        data?.properties?.periods[i]?.name.slice(0, 3) || "unknown",
+        data?.properties?.periods[i]?.shortForecast || "unknown",
+        data?.properties?.periods[i]?.probabilityOfPrecipitation?.value ||
+        "unknown",
+        data?.properties?.periods[i]?.windSpeed || "unknown",
+        data?.properties?.periods[i]?.windDirection || "unknown",
       );
 
       Days.push(day);
@@ -283,15 +276,16 @@ function praiseData(data) {
   }
 
   for (let i = 0; i <= 13; i++) {
-    if (data.properties.periods[i].isDaytime == false) {
+    if (data?.properties?.periods[i]?.isDaytime == false) {
       let day = new clsweatherDays(
-        data.properties.periods[i].temperature,
-        data.properties.periods[i].temperatureUnit,
-        data.properties.periods[i].name,
-        data.properties.periods[i].shortForecast,
-        data.properties.periods[i].probabilityOfPrecipitation.value,
-        data.properties.periods[i].windSpeed,
-        data.properties.periods[i].windDirection,
+        data?.properties?.periods[i]?.temperature || "unknown",
+        data?.properties?.periods[i]?.temperatureUnit || "unknown",
+        data?.properties?.periods[i]?.name || "unknown",
+        data?.properties?.periods[i]?.shortForecast || "unknown",
+        data?.properties?.periods[i]?.probabilityOfPrecipitation?.value ||
+        "unknown",
+        data?.properties?.periods[i]?.windSpeed || "unknown",
+        data?.properties?.periods[i]?.windDirection || "unknown",
       );
 
       Nights.push(day);
@@ -299,8 +293,8 @@ function praiseData(data) {
   }
 
   //DEBUG
-  console.log(Days);
-  console.log(Nights);
+  // console.log(Days);
+  // console.log(Nights);
 
   let weatherdata = new clsWeather(
     Days[0],
@@ -322,26 +316,46 @@ function praiseData(data) {
 }
 
 function getWeatherIcon(weather) {
-  if (weather.includes("Sunny")) {
-    return "☀️";
-  } else if (
-    weather.includes("Thunder") &&
-    (weather.includes("Rain") || weather.includes("Showers"))
-  ) {
-    return "⛈️";
-  } else if (weather.includes("Rain") || weather.includes("Showers")) {
-    return "🌧️";
-  } else if (weather.includes("Cloudy")) {
-    return "☁️";
-  } else if (weather.includes("Thunderstorm")) {
-    return "🌩️";
-  } else if (weather.includes("Snow")) {
-    return "🌨️";
-  } else if (weather.includes("Clear")) {
-    return "⛅";
-  } else if (weather.includes("Fog")) {
-    return "🌫️";
+  // if (weather.includes("Sunny")) {
+  //   return "☀️";
+  // } else if (
+  //   weather.includes("Thunder") &&
+  //   (weather.includes("Rain") || weather.includes("Showers"))
+  // ) {
+  //   return "⛈️";
+  // } else if (weather.includes("Rain") || weather.includes("Showers")) {
+  //   return "🌧️";
+  // } else if (weather.includes("Cloudy")) {
+  //   return "☁️";
+  // } else if (weather.includes("Thunderstorm")) {
+  //   return "🌩️";
+  // } else if (weather.includes("Snow")) {
+  //   return "🌨️";
+  // } else if (weather.includes("Clear")) {
+  //   return "⛅";
+  // } else if (weather.includes("Fog")) {
+  //   return "🌫️";
+  // }
+
+  const weatherIconMapping = {
+    "Thunder Rain Showers": "⛈️",
+    Sunny: "☀️",
+    Rain: "🌧️",
+    Showers: "🌧️",
+    Cloudy: "☁️",
+    Snow: "🌨️",
+    Clear: "🌈",
+    Fog: "🌫️",
+    Thunderstorm: "🌩️",
+  };
+
+  for (let condition in weatherIconMapping) {
+    if (weather.includes(condition)) {
+      return weatherIconMapping[condition];
+    }
   }
+
+  return "🌈";
 }
 
 function getWindDirectionIcon(windData) {
@@ -380,31 +394,34 @@ function displayWeatherDataForDay(
   ForecastVar,
   IconVar,
 ) {
-  TempVar.innerText = day.Temps + "° " + day.TempUnit;
-  HeaderVar.innerText = day.Header;
+  if (day.Header == "Tod") {
+    HeaderVar.innerText = "Today";
+  } else {
+    HeaderVar.innerText = day.Header;
+  }
+
   IconVar.innerText = day.Icon;
   ForecastVar.innerText = day.Forecast;
+  TempVar.innerText = day.Temps + "° " + day.TempUnit;
 }
 
 function displayWeatherAir(weather) {
   _todayPrecipitation.innerText =
     weather.Today.ProbabilityOfPrecipitation + "%";
+
   _todayWindSpeed.innerText = weather.Today.WindSpeed;
   _todayWindDirection.innerText = weather.Today.WindDirection;
   _todayWindIcon.className = getWindDirectionIcon(weather.Today.WindDirection);
-  // _todayWind.innerText = weather.Today.WindSpeed + ", " + weather.Today.WindDirection;
 }
 
 function displayWeatherHumidity(data) {
-  console.log(data);
-
   _todayHumidity.innerText = data.humidity + "%";
   _todayDewPoint.innerText = data.DewPoint.toFixed(2);
 }
 
 function displayWeatherForTodayHourly(hour, TempVar, HeaderVar, IconVar) {
-  TempVar.innerText = hour.Temps + "° " + hour.TempUnit;
   IconVar.innerText = hour.Icon;
+  TempVar.innerText = hour.Temps + "° " + hour.TempUnit;
 
   let date = new Date(hour.StartTime);
   HeaderVar.innerText = date.getHours() + ":00";
@@ -414,99 +431,40 @@ function displayWeatherForTodayHourly(hour, TempVar, HeaderVar, IconVar) {
 // page
 // ----------------------------------------------------------------------------------------------
 function displayHourlyData(HourlyData) {
-  displayWeatherForTodayHourly(
-    HourlyData[0],
-    _todaysTemp1,
-    _todaysHeader1,
-    _todaysIcon1,
-  );
-  displayWeatherForTodayHourly(
-    HourlyData[1],
-    _todaysTemp2,
-    _todaysHeader2,
-    _todaysIcon2,
-  );
-  displayWeatherForTodayHourly(
-    HourlyData[2],
-    _todaysTemp3,
-    _todaysHeader3,
-    _todaysIcon3,
-  );
-  displayWeatherForTodayHourly(
-    HourlyData[3],
-    _todaysTemp4,
-    _todaysHeader4,
-    _todaysIcon4,
-  );
-  displayWeatherForTodayHourly(
-    HourlyData[4],
-    _todaysTemp5,
-    _todaysHeader5,
-    _todaysIcon5,
-  );
-  displayWeatherForTodayHourly(
-    HourlyData[5],
-    _todaysTemp6,
-    _todaysHeader6,
-    _todaysIcon6,
-  );
+  for (let i = 0; i < 6; i++) {
+    displayWeatherForTodayHourly(
+      HourlyData[i],
+      HourlyTemps[i],
+      HourlyText[i],
+      HourlyIcon[i],
+    );
+  }
 
   displayWeatherHumidity(HourlyData[0]);
 }
 
 function displayData(weather) {
   //DEBUG
-  console.log(weather);
+  // console.log(weather);
 
-  displayWeatherDataForDay(
-    weather.Today,
-    _todayTemps,
-    _todayHeader,
-    _todayForecast,
-    _todayIcon,
-  );
-  displayWeatherDataForDay(
-    weather.Tomorrow,
-    _tomorrowTemps,
-    _tomorrowHeader,
-    _tomorrowForecast,
-    _tomorrowIcon,
-  );
-  displayWeatherDataForDay(
-    weather.NextTomorrow,
-    _nextTomorrowTemps,
-    _nextTomorrowHeader,
-    _nextTomorrowForecast,
-    _nextTomorrowIcon,
-  );
-  displayWeatherDataForDay(
-    weather.Fourth,
-    _fourthTemps,
-    _fourthHeader,
-    _fourthForecast,
-    _fourthIcon,
-  );
-  displayWeatherDataForDay(
-    weather.Fifth,
-    _fifthTemps,
-    _fifthHeader,
-    _fifthForecast,
-    _fifthIcon,
-  );
-  displayWeatherDataForDay(
-    weather.Sixth,
-    _sixthTemps,
-    _sixthHeader,
-    _sixthForecast,
-    _sixthIcon,
-  );
-  displayWeatherDataForDay(
-    weather.Seventh,
-    _seventhTemps,
-    _seventhHeader,
-    _seventhForecast,
-    _seventhIcon,
-  );
+  const keys = Object.keys(weather);
+  let dayIndex = 0;
+
+  for (let i = 0; i < keys.length && dayIndex < DaysTemps.length; i++) {
+    const key = keys[i];
+
+    if (!key.toLowerCase().endsWith("night")) {
+      displayWeatherDataForDay(
+        weather[key],
+        DaysTemps[dayIndex],
+        DaysText[dayIndex],
+        DaysForecast[dayIndex],
+        DaysIcons[dayIndex],
+      );
+
+      dayIndex++;
+    }
+  }
 
   displayWeatherAir(weather);
 }
@@ -518,7 +476,7 @@ function init() {
   let APIpromise = fetchWeatherDataFromAPI(APIstring);
   APIpromise.then(displayData).catch(onError).finally(done);
 
-  let HourlyAPIpromise = fetchHourlyWeatherDataFromAPI(TodayAPIstring);
+  let HourlyAPIpromise = fetchHourlyWeatherDataFromAPI(HourlyAPIstring);
   HourlyAPIpromise.then(displayHourlyData).catch(onError).finally(done);
 }
 
